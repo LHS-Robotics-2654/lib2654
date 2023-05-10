09d229119137cb843047a6e9eac42e7ff065087d
ad101-lab <55064247+alexDickhans@users.noreply.github.com>
2023-05-09 22:31:06 -0600

    Add noj

---
+++ html/classlib2654_1_1BooleanSensor.js
@@ -0,0 +1,11 @@
+var classlib2654_1_1BooleanSensor =
+[
+    [ "get", "classlib2654_1_1BooleanSensor.html#aaa9ed2834e6d02be6d3d6515ef590da0", null ],
+    [ "getFallingEdge", "classlib2654_1_1BooleanSensor.html#aef998243e05a13097fed427d624669a1", null ],
+    [ "getLastReadingAge", "classlib2654_1_1BooleanSensor.html#a5920b92e508464ae79017920cf6a858b", null ],
+    [ "getLastSensorReadingTimestamp", "classlib2654_1_1BooleanSensor.html#a9e401729710af85090de6f2bf9cfcc9d", null ],
+    [ "getRisingEdge", "classlib2654_1_1BooleanSensor.html#aefceb61661695efeacccc85bf49b7ae5", null ],
+    [ "update", "classlib2654_1_1BooleanSensor.html#a7dc0f87b058a3020365e8c002fc537b5", null ],
+    [ "updateLogic", "classlib2654_1_1BooleanSensor.html#a39e169737057f8c99e7e086b76c89e9d", null ],
+    [ "updateSensor", "classlib2654_1_1BooleanSensor.html#afe5a5c814abbb3b54bd6424640feaaf2", null ]
+];
---
+++ html/dir_35c289ce27de74f103711ed188f98082.js
@@ -0,0 +1,4 @@
+var dir_35c289ce27de74f103711ed188f98082 =
+[
+    [ "booleanSensor.hpp", "booleanSensor_8hpp_source.html", null ]
+];
